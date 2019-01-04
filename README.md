2dabf7b4aa0332afc5b0760b99d0c61c72bcab1d
Dave <daveyboi@gmail.com>
2019-01-04 21:28:13 +1300

    Added random off fucntionality and arduino file
    Cleaned up some unneccessary code
    Variablised some things
    Added random off time
    Added Arduino sketch file for esp8266
    Updated Readme with some todo's

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Lightcontrol
 
-A script to get the sunset time for the given GPS coordinates and send an on/off to a relay
+A ruby script to get the sunset time for the given GPS coordinates and send an on/off to an esp8266 relay
+
+Uses sunrise-sunset.org for the sunset times
+
+Designed to be run on a linux system running `atd`
+
+TODO:
+
+- Document esp8266 portion
+- Read in config file/command line flags
+- Add a sunrise function to allow a light to be turned on in the morning when required in winter
+- Error check ie what happens if the offtime is before the on time
+- Move this to run entirely on an ESP8266 rather than using at jobs
--- lightcontrol.rb
+++ lightcontrol.rb
@@ -1,9 +1,25 @@
+#!/usr/bin/env ruby
+
 require 'net/http'
 require 'json'
 require 'time'
 
-response = JSON.parse(Net::HTTP.get('api.sunrise-sunset.org', '/json?lat=-41.2865&lng=174.7762&date=today'))
-SUNSET = Time.parse(response['results']['sunset']) + Time.now.utc_offset
-SUNSET_HM = SUNSET.strftime("%H:%M")
-system"curl http://relay.home/on | at #{SUNSET_HM}"
+#Set Variables
+latitude='-41.1745'
+longitude='174.9820'
+nightOff='21:45'
+randomRange= 1800
+delay = rand(0..randomRange)
+
+#Get Sunset time from sunrise-sunset.org for given GPS coordinates
+response = JSON.parse(Net::HTTP.get('api.sunrise-sunset.org', "/json?lat=#{latitude}&lng=#{longitude}&date=today"))
+#Parse json response and extract the sunset time, adding the systemcs utc offset to obtain the local time of the sunset
+sunset = Time.parse(response['results']['sunset']) + Time.now.utc_offset
+#Create 'at' job on local linux box to switch relay on
+system"echo 'curl http://relay.home/on' | at #{sunset.strftime("%H:%M")}"
+
+#Parse given off time and add randomly selected delay
+offTime = Time.parse(nightOff) + delay
+#Create 'at' job on local linux box to switch relay on
+system"echo 'curl http://relay.home/on' | at #{offTime.strftime('%H:%M')}"
 
